6bc5d47584612695b4ae11eece41768a49265f14
Paul Casperson <casperson.paul@gmail.com>
2015-10-28 16:54:56 -0500

    seeded database

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap-sass'
 
 gem 'devise'
 
+gem 'faker'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+#create standard users
+10.times do
+  user = User.new(
+    name:        Faker::Name.name,
+    email:       Faker::Internet.email,
+    password:    Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+#create premium users
+3.times do
+  user = User.new(
+    name:       Faker::Name.name,
+    email:      Faker::Internet.email,
+    password:   Faker::Lorem.characters(10),
+    role:       "premium"
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+#create admin user
+admin = User.new(
+  name:       "Admin User",
+  email:      "admin@example.com",
+  password:   "helloworld",
+  role:       "admin"
+  )
+admin.skip_confirmation!
+admin.save!
+
+
+#create wicis
+100.times do
+  wici = Wici.create!(
+    user:      users.sample,
+    title:     Faker::Lorem.sentence,
+    body:      Faker::Lorem.paragraph
+    )
+end
+wicis = Wici.all
+
+puts "Seeding finished!"
+puts "#{User.count} users created"
+puts "#{Wici.count} wicis created"
+puts "Good job!"
